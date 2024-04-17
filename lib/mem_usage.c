3886221fcacaed888ffdf1cb38c3f1ea26b089ec
Romain Beauxis <toots@rastageeks.org>
2024-04-17 13:08:13 -0500

    Fix win build

--- lib/mem_usage.c
+++ lib/mem_usage.c
@@ -13,6 +13,22 @@
 #include <tchar.h>
 #include <windows.h>
 
+typedef struct _PROCESS_MEMORY_COUNTERS_EX2 {
+  DWORD   cb;
+  DWORD   PageFaultCount;
+  SIZE_T  PeakWorkingSetSize;
+  SIZE_T  WorkingSetSize;
+  SIZE_T  QuotaPeakPagedPoolUsage;
+  SIZE_T  QuotaPagedPoolUsage;
+  SIZE_T  QuotaPeakNonPagedPoolUsage;
+  SIZE_T  QuotaNonPagedPoolUsage;
+  SIZE_T  PagefileUsage;
+  SIZE_T  PeakPagefileUsage;
+  SIZE_T  PrivateUsage;
+  SIZE_T  PrivateWorkingSetSize;
+  ULONG64 SharedCommitUsage;
+} PROCESS_MEMORY_COUNTERS_EX2;
+
 CAMLprim value ocaml_mem_usage_mem_usage(value unit) {
   CAMLparam0();
   CAMLlocal1(ret);
